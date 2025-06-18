4890a5f6e324632c6339e2cde595888cfc29b140
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2025-06-18 14:55:14 -0500

    remove space mono loading code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler"; // 1. IMPORT THE WRAPPER
@@ -14,14 +13,6 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
 
   return (
     // 2. WRAP THE ENTIRE APP WITH THE GestureHandlerRootView
